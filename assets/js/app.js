0d257f237230be8ca748b974ab502a81c58c7736
Ijlal Ahmad EusapXai <31764427+eusapxai@users.noreply.github.com>
2020-08-04 13:42:58 +0500

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+$(function () {
+	$('[data-toggle="tooltip"]').tooltip();
+});
+
+$(document).ready(function () {
+	$(".spon-slider").owlCarousel({
+		dots: false,
+		loop: true,
+		autoplay: true,
+		autoplayHoverPause: true,
+		smartSpeed: 1000,
+		lazyLoad: true,
+		nav: true,
+		margin: 0,
+		navText: ["<", ">"],
+		responsive: {
+			0: { items: 2 },
+			1500: { items: 6 },
+			1200: { items: 5 },
+			992: { items: 3 },
+			700: { items: 2 },
+		},
+	});
+});
