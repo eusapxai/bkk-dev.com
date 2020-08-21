4fbb8e65af60840c830144c27d3d9c753d02d80c
Ijlal Ahmad EusapXai <31764427+eusapxai@users.noreply.github.com>
2020-08-21 09:42:44 +0500

    update

--- sentmail.php
+++ sentmail.php
@@ -1,7 +1,6 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-
 //include guzzle http
 require 'guzzle/autoload.php';
 
@@ -12,25 +11,39 @@ $responseCaptcha = $request->response;
 $name = $request->name;
 $email = $request->email;
 $message = $request->message;
-$client = new \GuzzleHttp\Client();
-$res = $client->request('POST', 'https://www.google.com/recaptcha/api/siteverify', [
+$body = "Name: " . $name . ' Email: ' . 'Message: ' . $message;
+// $client = new \GuzzleHttp\Client();
+// $res = $client->request('POST', 'https://www.google.com/recaptcha/api/siteverify', [
+//     'form_params' => [
+//         'secret' => $secret,
+//         'response' => $responseCaptcha,
+//     ],
+//     'connect_timeout' => 5,
+// ]);
+// $http_code = $res->getStatusCode();
+// $response = json_decode($res->getBody(), true);
+// if ($http_code == 200 && $response['success']) {
+//send email
+$recipientEmail = 'ijlal.ahmad@switchsolutions.com.pk';
+$res = $client->request('POST', 'http://api.bakhaberkissan.com/v1/api_email.php', [
     'form_params' => [
-        'secret' => $secret,
-        'response' => $responseCaptcha,
+        'email' => $recipientEmail,
+        'subject' => 'Bkk website contact',
+        'message' => $body,
     ],
     'connect_timeout' => 5,
 ]);
 $http_code = $res->getStatusCode();
 $response = json_decode($res->getBody(), true);
-if ($http_code == 200 && $response['success']) {
-    //send email
-    return response()->json([
-        'statusCode' => 200,
-        'status' => 'captcha valid',
-    ], 200);
+if ($http_code == 200 && $response['status'] == 1) {
+    $data = ['statusCode' => '200', 'status' => 'success'];
+    return json_encode($data);
 } else {
-    return response()->json([
-        'statusCode' => 200,
-        'status' => 'captcha invalid',
-    ], 200);
+    $data = ['statusCode' => '400', 'status' => 'error'];
+    return json_encode($data);
 }
+
+// } else {
+//     $data = ['statusCode' => '401', 'status' => 'error'];
+//     return json_encode($data);
+// }
